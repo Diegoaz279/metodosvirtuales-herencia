8dc41b5e716e342fc4830c20b70087e504e15ac5
Diego Castillo <Diegoaz279@gmail.com>
2024-06-14 16:46:03 -0400

    Agregar archivos de proyecto.

---
+++ metodosvirtuales/Cono.cs
@@ -0,0 +1,27 @@
+﻿using Metodosvirtuales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodosvirtuales
+{
+    public class Cono : CuerpoRevolucion
+
+    {
+        private double altura, generatriz;
+
+        public Cono(double radio, double altura, double generatriz) : base(radio)
+        {
+            this.altura = altura;
+            this.generatriz = generatriz;
+        }
+
+        public override double Volumen()
+        {
+            return (1.0 / 3) * base.Volumen() * altura;
+        }
+
+    }
+}
